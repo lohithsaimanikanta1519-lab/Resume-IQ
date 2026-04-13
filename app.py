67997ecfc9032ec7305f8ad721e8e493857b0a20
Lohith <your-email@gmail.com>
2026-04-13 11:48:57 +0530

    Updated for deployment

--- app.py
+++ app.py
@@ -219,6 +219,7 @@ def screen():
 
 
 if __name__ == "__main__":
-    print("\n🚀  ResumeIQ is running at http://127.0.0.1:5000\n")
-    app.run(debug=True, port=5000)
-    
+    print("\n🚀  ResumeIQ is running...\n")
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
